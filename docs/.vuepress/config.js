bebbebc205b63db3236d0ddd940ce6f20b5bd77d
zhangxueping <zhangxp@iraytek.com>
2022-03-10 17:05:15 +0800

    添加导航栏

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,19 @@
 module.exports = {
-    base:"/irayui/",
-    themeConfig: {
-      sidebar: [
-        '/',
-        '/componentDocs/card'
-      ]
-    }
+  title:"Iray-ui",
+  base:"/irayui/",
+  themeConfig: {
+    nav:[
+      {
+        text:'Home',link:'/'
+      },{
+        text:'Github',link:'https://github.com/gaiery/irayui/tree/master'
+      },{
+        text:'npm',link:'https://www.npmjs.com/package/iray-ui'
+      }
+    ],
+    sidebar: [
+      '/',
+      '/componentDocs/card'
+    ]
   }
+}
--- vue.config.js
+++ vue.config.js
@@ -5,12 +5,5 @@ module.exports = {
             template:'public/index.html',
             filename: 'index.html'
         }
-    },
-    css: {
-        loaderOptions: {
-          sass: { // 加载全局scss文件
-            // additionalData: '@import "@/component/css/common.scss";'
-          }
-        }
     }
 }
